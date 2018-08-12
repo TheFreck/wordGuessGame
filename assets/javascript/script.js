4e075bde95ba2442f834b0276e109c55d3001c17
Wilhelm Picaco <wpicaco@gmail.com>
2018-08-12 15:56:31 -0600

    basic javascript functionality

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,16 +1,17 @@
-var word;
-var realWordLength=0;
-var wordSpaces;
-var word;
 
 function begin(){
-    
     console.log("begin");
+
+    //initialize
+    document.getElementById("outcome").innerHTML="";
+    
+    //create a list of potential words
+    
     var wordList = [
         "LED_ZEPPELIN",
         "PINK_FLOYD",
-        "THE_DOORS",
-        "THE_BEETLES",
+        "DOORS",
+        "BEATLES",
         "VIOLENT_FEMMES",
         "PIXIES",
         "JANE'S_ADDICTION",
@@ -23,59 +24,152 @@ function begin(){
         "PEARL_JAM",
         "SOUND_GARDEN",
         "DEF_LEOPARD",
-        ""
+        "NINE_INCH_NAILS",
+        "DEVIL_MAKES_THREE",
+        "RAILROAD_EARTH",
+        "TRAMPLED_BY_TURTLES",
+        "HOT_BUTTERED_RUM",
+        "JACKSON_FIVE",
+        "CANNED_HEAT",
+        "RUSH",
+        "BLACK_SABBATH",
+        "BEASTIE_BOYS",
+        "RUN_DMC",
+        "EMINEM",
+        "FUGIES",
+        "DIGABLE_PLANET",
+        "MASSIVE_ATTACK",
+        "PORTISHEAD",
+        "THIEVERY_CORPORATION",
+        "ABBA",
+        "SHPONGLE",
+        "DEFTONES",
+        "CLUTCH",
+        "DEAD_KENNEDIES",
+        "BAD_RELIGION",
+        "SEX_PISTOLS",
+        "VANDALS",
+        "REM",
+        "VELVET_UNDERGROUND",
+        "SUBLIME",
+        "CREAM",
+        "TALKING_HEADS",
+
+        
     ];
+    
+    var word = "";
+    var realWordLength = 0;
+    var wordSpaces = [];
+    var wordLength = 0;
+    
+    //get a random item from the list
+    
     var wordIndex = Math.floor(Math.random() * wordList.length);
     word = wordList[wordIndex];
     console.log(word);
     wordLength = word.length;
+    var rejectArray=[];
     
+    //convert the word string into an array
+    
+    var wordArray=[];
+    for(i=0; i<wordLength; i++){
+        wordArray.push(word[i]);
+    }
+    
+    //turn the letters into blank spaces
     
     for(i=0; i<wordLength; i++){
         if(word[i] === "_"){
-            wordSpaces = wordSpaces + "&nbsp";
+            wordSpaces.push("&nbsp");
         }else if(word[i] === "'"){
             
-            wordSpaces = wordSpaces + word[i];
+            wordSpaces.push(word[i]);
         }else{
-            wordSpaces = wordSpaces + " _ ";
-            realWordLength = realWordLength + 1;
+            wordSpaces.push(" _ ");
+            realWordLength += 1;
             
         }
     }
+    var guessesLeft=10;
+    document.getElementById("remaining").innerHTML=`Guesses left: ${guessesLeft}`;
+    
+//turn the spaces back into a string
 
+    var wordString="";
+    for(i=0; i<wordSpaces.length; i++){
+        wordString+=wordSpaces[i];
+    }
 
-    document.getElementById("instructions").innerHTML=`This is a ${realWordLength} letter word`;
+//print the spaces string on the screen
+
+    document.getElementById("instructions").innerHTML=`There are ${realWordLength} letters int this band name`;
     
     
+    document.getElementById("spaces").innerHTML=wordString;
     
-    
-    
-    document.getElementById("spaces").innerHTML=wordSpaces;
-    console.log(wordSpaces);
-    wordSpaces.splice(0,1);
-    console.log(wordSpaces);
-}
+//put the rejected letters in the reject div
 
+    document.getElementById("rejects").innerHTML=rejectArray;
 
+//event listener
 
+    document.onkeyup=function(event){
+        var guess = event.key;
+        console.log("guess ", guess);
+        var Guess = guess.toUpperCase();
+        console.log("Guess ", Guess)
+        var k = 0;
 
-
-document.onkeyup=function(event){
-    var guess=event.key;
-    console.log("guess " + guess);
-    var Guess=guess.toUpperCase();
-    console.log("Guess " + Guess)
-
-    for(i=0; i<realWordLength; i++){
-        if(Guess===word[i]){
-            wordSpaces.slice(i, 1, word[i]);   
-            console.log(wordSpaces);
-        }else{
-            console.log(wordSpaces);
+    //is the letter correct? if so post it
+        
+        for(i=0; i<word.length; i++){
+            if(Guess===word[i]){
+                wordSpaces.splice(i,1,word[i]);
+                k++;
+                console.log("k",k);
+                console.log("wordSpaces",wordSpaces);
+            }
+        }
+       
+        if(k === 0){
+            guessesLeft -= 1;
+            rejectArray.push(Guess);
+            document.getElementById("rejects").innerHTML=`Dustbin: ${rejectArray}`;
         }
         
-    }
-    document.getElementById("spaces").innerHTML=wordSpaces;
-}
 
+        
+        document.getElementById("remaining").innerHTML=`Guesses 
+        left: ${guessesLeft}`;
+
+    //turn it back into a string
+        wordString="";
+        console.log("wordString",wordString);
+        
+        for(i=0; i<wordSpaces.length; i++){
+            
+            wordString += wordSpaces[i];
+        }
+
+        console.log("final wordString",wordString);
+
+    //post it with guessed letters revealed
+
+        document.getElementById("spaces").innerHTML=wordString; 
+        
+        //test to see if win or loss
+        
+        var n = wordString.includes("_");
+        if(!n){
+            document.getElementById("outcome").innerHTML="You WIN!!!";
+        
+        }
+        
+        if(guessesLeft <= 0){
+            document.getElementById("outcome").innerHTML="You Lose";
+        }
+    };
+
+};
